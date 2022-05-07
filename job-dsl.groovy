b3082040603d176bd3d82a0f32e403592860bcdb
Đào Đức Quang <>
2022-05-07 16:06:03 +0700

    add Job dsl

---
+++ job-dsl.groovy
@@ -0,0 +1,16 @@
+pipelineJob('Call For Service') {
+
+    def repo = 'https://github.com/tintin0122/call_for_service.git'
+
+    triggers {
+        scm('*/15 * * * *')
+    }
+
+    definition {
+        cpsScm {
+            scm {
+                git(repo, 'main', { node -> node / 'extensions' << '' } )
+            }
+        }
+    }
+}
